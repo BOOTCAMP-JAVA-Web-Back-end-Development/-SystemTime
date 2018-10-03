4f21b6b3b6b4e570b4b643cf41e904e80aa27ea1
thanhdc2018 <hung9dquangtrung>
2018-10-03 10:57:54 +0700

    ok

---
+++ src/SystemTime.java
@@ -0,0 +1,7 @@
+import java.util.Date; // đưa vào 1 lớp date
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
